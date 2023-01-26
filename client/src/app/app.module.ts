839443442d29edb646bd7774d3008242dc5d701e
Dor Bezalel <dor.netcraft@gmail.com>
2023-01-26 21:53:00 +0200

    Initial Commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskComponent } from './components/task/task.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasksComponent,
+    TaskComponent,
+    LoginComponent,
+    ProfileComponent,
+    NavBarComponent,
+    RegisterComponent,
+    AddTaskComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
